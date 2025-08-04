646e7b2f7297df13146af01edec60a0e686db0dc
Mert <62549656+fswair@users.noreply.github.com>
2025-08-04 03:22:16 +0300

    Update logger.py

--- logger.py
+++ logger.py
@@ -17,7 +17,7 @@ class BaseConsole(Console):
         super().__init__(*args, **kwargs)
         self.fmt = "[bold green]{loglevel}[/]:     {message}"
 
-    def logger(self, loglevel: Literal['INFO', 'WARN', 'KILL'] = 'INFO', *, message: str | None = None, color: str = 'green', bold: bool = True):
+    def logger(self, loglevel: Literal['INFO', 'WARN'] = 'INFO', *, message: str | None = None, color: str = 'green', bold: bool = True, bold_level: bool = True) -> None:
         """
         Log a message with the specified log level, color, and bold formatting.
         """
@@ -27,21 +27,21 @@ class BaseConsole(Console):
             message = f"[bold {color}]{message}[/]"
         else:
             message = f"[{color}]{message}[/]"
-        loglevel = f"[{color_map.get(loglevel, 'green')}]{loglevel}[/]"
+        loglevel = f"[{color_map.get(loglevel, 'green')}]{loglevel}"
         return self.print(self.fmt.format(loglevel=loglevel, message=message))
-    
-    def info(self, message: str | None = None, color: str = 'green', bold: bool = True):
+
+    def info(self, message: str | None = None, color: str = 'green', bold: bool = True, bold_level: bool = True):
         """
         Log an info message.
         """
-        return self.logger('INFO', message=message, color=color, bold=bold)
-    
+        return self.logger('INFO', message=message, color=color, bold=bold, bold_level=bold_level)
+
     def warn(self, message: str | None = None, color: str = 'yellow', bold: bool = True):
         """
         Log a warning message.
         """
         return self.logger('WARN', message=message, color=color, bold=bold)
-    
+
     def kill(self, message: str | None = None, color: str = 'red', bold: bool = True):
         """
         Placeholder for a method to kill the console.
@@ -56,10 +56,14 @@ class VenusConsole(BaseConsole):
     """
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.venus_fmt = "[green]{time}[green]   {message}"
+        self.venus_fmt = "[green]{time}[green] {message}"
 
-    def log(self, message: str):
+    def log(self, message: str, bold: bool = False, color: str = 'green'):
         """
         Log a message with the current time.
         """
+        if bold:
+            message = f"[bold {color}]{message}[/]"
+        else:
+            message = f"[{color}]{message}[/]"
         self.print(self.venus_fmt.format(time=datetime.now().strftime('%H:%M:%S.%f')[:-3], message=message))
